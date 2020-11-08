61f7882c37aa2236f2f4301d6156017f8b79b60d
17hoehbr <17hoehbr@gmail.com>
2020-11-07 22:19:54 -0500

    modified to support wine-tkg instead

--- updatePGEfast.sh
+++ updatePGEfast.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-baseuri="https://github.com/GloriousEggroll/proton-ge-custom/releases/download"
-latesturi="https://api.github.com/repos/GloriousEggroll/proton-ge-custom/releases/latest"
-dstpath="$HOME/.steam/root/compatibilitytools.d"
-
+baseuri="https://github.com/Frogging-Family/wine-tkg-git/releases/download"
+latesturi="https://api.github.com/repos/Frogging-Family/wine-tkg-git/releases/latest"
+dstpath="$HOME/.steam/root/compatibilitytools.d/"
 
   latestversion="$(curl -s $latesturi | egrep -m1 "tag_name" | cut -d \" -f4)"
-  if [[ -d $dstpath/Proton-$latestversion ]]
+  if [[ -d $dstpath/proton_tkg_$latestversion ]]
   then
     echo "Proton $latestversion is the latest version and is already installed."
     echo "Exiting..."
@@ -13,7 +12,7 @@ dstpath="$HOME/.steam/root/compatibilitytools.d"
   else
     echo "Proton $latestversion is the latest version and is not installed yet."
     echo "Installing Proton $latestverion"
-    url=$(curl -s $latesturi | egrep -m1 "browser_download_url.*Proton" | cut -d \" -f4)
+    url=$(curl -s $latesturi | egrep -m1 "browser_download_url.*proton" | cut -d \" -f4)
   fi
 
 rsp="$(curl -sI "$url" | head -1)"
@@ -27,4 +26,5 @@ echo "$rsp" | grep -q 302 || {
     echo [Info] Created "$dstpath"
 }
 
-curl -sL "$url" | tar xfzv - -C "$dstpath"
+curl -sLo "$dstpath/proton_tkg_$latestversion.zip" "$url"
+unzip -q "$dstpath/proton_tkg_$latestversion.zip" -d "$dstpath/proton_tkg_$latestversion/"
